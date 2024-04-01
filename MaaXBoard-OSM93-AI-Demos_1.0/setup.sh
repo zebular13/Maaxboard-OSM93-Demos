32831e789137a4b54db58fb0048123c35f9f5188
jacob <jcase.bus@gmail.com>
2024-04-01 09:19:21 -0700

    Remove enable-scripts.sh and update setup.sh pip3 list to resolve graphics lag issue.

--- MaaXBoard-OSM93-AI-Demos_1.0/setup.sh
+++ MaaXBoard-OSM93-AI-Demos_1.0/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66a1db345c0f3bfb095354af761df3565602443f4aac775fa146ef699e3315a6
-size 982
+oid sha256:980fa00c41a9a1012f9e756ce5b986f96bbcdd254a20a18da552c2504037f999
+size 1005
