Update demo suite to v1.6. Correct CPU vs NPU inference time averaging. Added in InferenceTimeLogger class to dms folder to handle operations around collecting model inference times.

MaaXBoard-OSM93-Demo_Suite_1.6/dms/face_detection.py
@@ -7,8 +7,9 @@
 import math
 import cv2
 import numpy as np
+import time
 from dms.utils import nms_oneclass
-
+from dms.inference_timer import InferenceTimeLogger
 
 FACE_MODEL_3D = np.array([
     (-165.0, 170.0, -135.0),  # left eye
@@ -31,6 +32,9 @@ class FaceDetector:
                  target_height=128,
                  run_on_hardware = False
                  ):
+
+    
+        self.inference_logger = InferenceTimeLogger()
         
         if run_on_hardware:
             import tflite_runtime.interpreter as tflite
@@ -79,7 +83,12 @@ class FaceDetector:
 
         # invoke
         self.interpreter.set_tensor(self.input_idx, input_data)
+        start = time.time()
         self.interpreter.invoke()
+        end = time.time()
+        delta = end-start
+        self.inference_logger.face_landmark_inf_time = delta
+        # print("Face detection inference time:", delta)
         scores = self.interpreter.get_tensor(self.outputs_idx['classificators']).squeeze()
         scores = 1 / (1 + np.exp(-scores))
         bboxes = self.interpreter.get_tensor(self.outputs_idx['regressors']).squeeze()